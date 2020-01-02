e60ffec9b8a84601c3b5c019ccdc1a1e8c47055e
JustHedgehog <seba3444gaming@gmail.com>
2020-01-02 20:19:58 +0100

    dodanie komentarzy opisujacych gdzie co powklejac

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <iostream>
 
+//nad tym wrzucie linki do plikow
+
+
 int main()
 {
 
-	printf("Witamy w wielkim swiecie");
+	printf("Witamy w wielkim swiecie \n");
+	
+	//tutaj wrzuccie funkcje
 
 	return 0;
 
