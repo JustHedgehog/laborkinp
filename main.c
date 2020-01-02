d472087387d50b8edc6b6be676b063a61cf1f799
Frixenos <frixenos@gmail.com>
2020-01-02 20:31:44 +0100

    dodanie funkcji mnozenia

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//nad tym wrzucie linki do plikow
+
+
+int main()
+{
+
+	printf("Witamy w wielkim swiecie \n");
+
+ // mnozenie
+ float mnozenie(float a, float b){
+ float c=a*b;
+ return c;
+ }
+
+ printf("\nWynik=%.2f", mnozenie(5, 8));
+	return 0;
+
+
+}
